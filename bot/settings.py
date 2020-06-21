1f8ae9ab9a318dfca7592e7c2b675c1a16b1b864
damiso15 <damiso15@yahoo.com>
2020-06-21 12:58:45 +0100

    Adam Version 1.0

--- bot/settings.py
+++ bot/settings.py
@@ -26,9 +26,9 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['**.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['**.herokuapp.com', '127.0.0.1',]
 
 
 # Application definition
